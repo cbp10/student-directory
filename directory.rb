9629d2faad4f3ed252e59afaaa39ae9678b82082
Rashika Patel <rashika_patel@hotmail.com>
2018-08-01 13:02:31 +0100

    Add save student method

--- directory.rb
+++ directory.rb
@@ -30,6 +30,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the students to students.csv"
   puts "9. Exit"
 end
 
@@ -39,6 +40,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -52,6 +55,15 @@ def show_students
   print_footer
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 def interactive_menu
   loop do
